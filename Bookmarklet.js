1c214d20ae95dd75b0c5bc0b4cbedb40d6dfcc3a
My-Altt <101564533+My-Altt@users.noreply.github.com>
2022-12-13 11:38:08 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,1 +1,1 @@
-
+javascript:(function()%7Basync function fetch_demo()%0A%7B%0A%09const resp %3D await fetch('https%3A%2F%2Fapi.dictionaryapi.dev%2Fapi%2Fv2%2Fentries%2Fen%2Fdog')%3B%0A%0A%09console.log(await resp.json())%3B%0A%7D%0A%0Afetch_demo()%3B%7D)()%3B
